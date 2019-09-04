14a67edad2e2ca790c3a17c5b516fb439fd229e4
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-04 16:18:43 -0700

    Get/:locationId gets location with array of temps

--- test/temps.test.js
+++ test/temps.test.js
@@ -5,6 +5,7 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const Location = require('../lib/models/Location');
+const Temp = require('../lib/models/Temperature');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -15,13 +16,24 @@ describe('app routes', () => {
     return mongoose.connection.dropDatabase();
   });
 
+  let loc1;
   beforeEach(async() => {
-    JSON.parse(JSON.stringify(await Location.create({
+    loc1 = JSON.parse(JSON.stringify(await Location.create({
       name: 'Mars'
     })));
 
     JSON.parse(JSON.stringify(await Location.create({
       name: 'Jason\'s Dining Hall'
+    })));
+
+    JSON.parse(JSON.stringify(await Temp.create({
+      temperature: 12.5,
+      location: loc1._id
+    })));
+
+    JSON.parse(JSON.stringify(await Temp.create({
+      temperature: 74,
+      location: loc1._id
     })));
   });
 
@@ -40,6 +52,19 @@ describe('app routes', () => {
         }, {
           _id: expect.any(String),
           name: 'Jason\'s Dining Hall',
+          __v: 0
+        }]);
+      });
+  });
+
+  it('Get/:locationId gets all temperatures by location id', () => {
+    return request(app)
+      .get(`/api/v1/temps/${loc1._id}`)
+      .then(res => {
+        expect(res.body).toEqual([{
+          _id: loc1._id,
+          name: loc1.name,
+          temps: ['12.5', '74'],
           __v: 0
         }]);
       });
