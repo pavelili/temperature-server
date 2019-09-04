2b82f9a96637a8e52b860cb0c62c36a0d6c1c162
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-04 11:28:48 -0700

    deregister is giving the right status

--- test/temperature.test.js
+++ test/temperature.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Location = require('../lib/models/Location');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -18,7 +19,7 @@ describe('app routes', () => {
     return mongoose.connection.close();
   });
 
-  it('returns a status code of 204 when POST route is hit', () => {
+  it('status route returns a status code of 204 when POST route is hit', () => {
     return request(app)
       .post('/status')
       .then(res => {
@@ -26,7 +27,7 @@ describe('app routes', () => {
       });
   });
 
-  it('returns a status code of 200 and an object with id', () => {
+  it('register returns a status code of 200 and an object with id', () => {
     return request(app)
       .post('/register')
       .send({
@@ -37,6 +38,20 @@ describe('app routes', () => {
         expect(res.body).toEqual({
           id: expect.any(String)
         });
+      });
+  });
+
+  it('deregister returns a status code of 204', async() => {
+    const location = JSON.parse(JSON.stringify(await Location.create({
+      name: 'Jupiter'
+    })));
+    return request(app)
+      .delete('/deregister')
+      .send({
+        id: location._id
+      })
+      .then(res => {
+        expect(res.status).toEqual(204);
       });
   });
 });
