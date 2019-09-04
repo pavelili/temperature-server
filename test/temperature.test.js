412169658d44763740433a4a67ef6c56c9ff0962
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-04 11:01:37 -0700

    register route adds location to database

--- test/temperature.test.js
+++ test/temperature.test.js
@@ -2,27 +2,41 @@ require('dotenv').config();
 
 const request = require('supertest');
 const app = require('../lib/app');
-// const connect = require('../lib/utils/connect');
-// const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
 
 describe('app routes', () => {
-  // beforeAll(() => {
-  //   connect();
-  // });
+  beforeAll(() => {
+    connect();
+  });
 
-  // beforeEach(() => {
-  //   return mongoose.connection.dropDatabase();
-  // });
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
 
-  // afterAll(() => {
-  //   return mongoose.connection.close();
-  // });
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
 
   it('returns a status code of 204 when POST route is hit', () => {
     return request(app)
       .post('/status')
       .then(res => {
         expect(res.status).toEqual(204);
+      });
+  });
+
+  it('returns a status code of 200 and an object with id', () => {
+    return request(app)
+      .post('/register')
+      .send({
+        name: 'Mars'
+      })
+      .then(res => {
+        expect(res.status).toEqual(200);
+        expect(res.body).toEqual({
+          id: expect.any(String)
+        });
       });
   });
 });
